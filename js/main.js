c9e48724062483910b0e0d00288f0fa215790bd8
yogesh-karki <yogesh@hueshine.com>
2022-01-17 13:36:05 +0545

    slide owl carousel

--- js/main.js
+++ js/main.js
@@ -32,35 +32,17 @@ menuBtn.addEventListener('click',(e) => {
 // })
 }
 
-var owl;
-    function customPager() {
+function customPager () {
+    console.log("changes")
+}
 
-        $.each(this.owl.userItems, function (i) {
-
-            var titleData = jQuery(this).find('h3').text();
-            var paginationLinks = jQuery('.owl-controls .owl-pagination .owl-page span');
-
-            $(paginationLinks[i]).append(titleData).wrap('<h3 class="slideTitle"></h3>');
-
-        });
-    }
 
 $('.slide').owlCarousel({
     autoPlay: 4000,
     navigation: true,
-    slideSpeed: 3000,
-    paginationSpeed: 2000,
-    singleItem: true,
-    afterInit: customPager,
-    afterUpdate: customPager,
-    navigationText : false,
     loop: true,
-    margin: 0,
-    nav: false,
-    autoplay:true,
     dots: true,
-    animateOut: 'fadeOut',
-    animateIn: 'fadeIn',
+    dotsData: true,
     autoplayTimeout: 6000,
     responsive: {
         0: {
