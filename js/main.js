f5737a99492fc7fc5c518bdcc8a438815925696a
neu-astha <neuastha12@gmail.com>
2022-01-17 13:09:33 +0545

    code

--- js/main.js
+++ js/main.js
@@ -1,18 +1,22 @@
-const header = document.querySelector(".header");
+// const header = document.querySelector(".header");
 
-$(window).on('scroll', function() {
-    if ($(this).scrollTop() >80){  
-        header.classList.add('solid-header')
-    }
-    else{
-        header.classList.remove('solid-header')
-    }
-});
+// $(window).on('scroll', function() {
+//     if ($(this).scrollTop() >80){  
+//         header.classList.add('solid-header')
+//     }
+//     else{
+//         header.classList.remove('solid-header')
+//     }
+// });
+
+
 
 
 // Responsive Navigation
 
 const menuBtn = document.querySelector('.menu-btn');
+if(menuBtn) {
+    
 const navBox = document.querySelector('.navs')
 
 menuBtn.addEventListener('click',(e) => {
@@ -22,9 +26,65 @@ menuBtn.addEventListener('click',(e) => {
     navBox.classList.toggle("open")
 })
 
+// // closeBtn.addEventListener('click',(e) => {
+// //     e.preventDefault();
+// //     navBox.classList.remove("open")
+// })
+}
+
+var owl;
+    function customPager() {
+
+        $.each(this.owl.userItems, function (i) {
+
+            var titleData = jQuery(this).find('h3').text();
+            var paginationLinks = jQuery('.owl-controls .owl-pagination .owl-page span');
+
+            $(paginationLinks[i]).append(titleData).wrap('<h3 class="slideTitle"></h3>');
+
+        });
+    }
+
+$('.slide').owlCarousel({
+    autoPlay: 4000,
+    navigation: true,
+    slideSpeed: 3000,
+    paginationSpeed: 2000,
+    singleItem: true,
+    afterInit: customPager,
+    afterUpdate: customPager,
+    navigationText : false,
+    loop: true,
+    margin: 0,
+    nav: false,
+    autoplay:true,
+    dots: true,
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    autoplayTimeout: 6000,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+})
+
+
+let popUp = document.querySelector(".popup");
+let closeBtn = document.querySelector(".close-btn");
+let page = document.querySelector(".full");
+
+popUp.addEventListener('click', ()=> {
+    page.classList.toggle('active')
+})
 closeBtn.addEventListener('click',(e) => {
-    e.preventDefault();
-    navBox.classList.remove("open")
+    page.classList.remove("active")
 })
 
 
